b1aa18309288cbae74d38352a2606c9cbcce5844
Neil Hiddink <nhiddink@Neils-Mac-2.attlocal.net>
2016-04-25 19:26:15 -0400

    Added Pokemon Image Resources

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Neil Hiddink on 4/25/16.
+//  Copyright © 2016 Neil Hiddink. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    var name:String!
+    var dexNumber: Int!
+    
+    
+    init(name: String, dexNumber: Int){
+    
+        self.name = name
+        self.dexNumber = dexNumber
+    
+    
+    }
+    
+    init(){
+    
+        
+        
+    }
+
+}
