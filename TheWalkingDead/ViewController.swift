c269568afb9d054424cf432d339af35b68ccd884
Neil Hiddink <nhiddink@Neils-Mac-2.attlocal.net>
2016-05-03 23:30:52 -0400

    Set Up Project and Imported Resources

--- TheWalkingDead/ViewController.swift
+++ TheWalkingDead/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
